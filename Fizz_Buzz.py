ecc52ba9fadeeaa91b75bd678c517162e051f964
Harsh Khandelwal <63328220+HarshKhandelwal1552@users.noreply.github.com>
2021-01-04 14:23:31 +0530

    Delete Fizz_Buzz.py

--- Fizz_Buzz.py
+++
@@ -1,9 +0,0 @@
-for i in range(1, 101):
-    if i % 5 == 0 and i % 3 == 0: # checking the divisibility with 15
-        print("FizzBuzz")
-    elif i % 3 == 0: # checking the divisibility with 3
-        print("Fizz")
-    elif i % 5 == 0: # checking the divisibility with 5
-        print("Buzz")
-    else:
-        print(i)
